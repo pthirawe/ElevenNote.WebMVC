2a48f9ddfcfd46cf3c144259e393b14202674c66
Patom Thirawetchayan <patomt@gmail.com>
2022-02-09 14:05:33 -0500

    updated note index view

---
+++ ElevenNote.Data/Migrations/202202091825532_UpdateNoteOwnerID.cs
@@ -0,0 +1,18 @@
+﻿namespace ElevenNote.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateNoteOwnerID : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Note", "OwnerId", c => c.Guid(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Note", "OwnerId", c => c.Int(nullable: false));
+        }
+    }
+}
